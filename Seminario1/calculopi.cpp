e9eb9bd751928a7b1b1bbd18c5f5d00a3cf4c40b
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-04 23:39:27 +0200

    Primeros toques a calculopi.cpp

--- Seminario1/calculopi.cpp
+++ Seminario1/calculopi.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <pthread.h>
+#include "fun_tiempo.h"
 
 using namespace std ;
 
@@ -20,29 +21,34 @@ double resultado_parcial[n] ; // tabla de sumas parciales (una por hebra)
 // ---------------------------------------------------------------------
 // implementa función $f$
 
-double f( double x )       
-{ 
+double f( double x )
+{
    return 4.0/(1+x*x) ;     // $~~~~f(x)\,=\,4/(1+x^2)$
 }
 // ---------------------------------------------------------------------
 // cálculo secuencial
 
 double calcular_integral_secuencial( )
-{  
+{
+  struct timespec inicio = ahora();
    double suma = 0.0 ;                      // inicializar suma
    for( unsigned long i = 0 ; i < m ; i++ ) // para cada $i$ entre $0$ y $m-1$
       suma += f( (i+0.5)/m );               // $~~~~~$ añadir $f(x_i)$ a la suma actual
+      struct timespec fin = ahora();
+      cout<< "Tiempo Secuencial: "<< duracion (&inicio, &fin) << " segundos."<<endl;
    return suma/m ;                          // devolver valor promedio de $f$
 }
 // ---------------------------------------------------------------------
 // función que ejecuta cada hebra
 
-void * funcion_hebra( void * ih_void ) 
-{  
+void * funcion_hebra( void * ih_void )
+{
    unsigned long ih = (unsigned long) ih_void ; // número o índice de esta hebra
    double sumap = 0.0 ;
    // calcular suma parcial en "sumap"
-   //.....
+   for (unsigned long i = ih; i < m; i+=n){
+     sumap += f( ((i+0.5)/m));
+   }
    resultado_parcial[ih] = sumap ; // guardar suma parcial en vector.
    return NULL ;
 }
@@ -50,30 +56,35 @@ void * funcion_hebra( void * ih_void )
 // cálculo concurrente
 
 double calcular_integral_concurrente( )
-{  
+{
+  struct timespec inicio = ahora();
+  double sum;
    // crear y lanzar $n$ hebras, cada una ejecuta "funcion\_concurrente"
-   //.....
+  pthread_t id_hebra[n];
+  for(unsigned i = 0; i < n; i++){
+		pthread_create( &(id_hebra[i]),NULL,funcion_hebra,(void *) i);
    // esperar (join) a que termine cada hebra, sumar su resultado
-   //.....
+   Sum += resultado_parcial[i];
    // devolver resultado completo
-   // .....
-   return 0.0 ; // (cambiar)
+   struct timespec fin = ahora();
+ 	cout << "Tiempo Concurrente " << duracion( &inicio, &fin) << " seg." << endl;
+   return Sum/m;
 }
 
 // ---------------------------------------------------------------------
 
 int main()
 {
-   
+
    cout << "Ejemplo 4 (cálculo de PI)" << endl ;
    double pi_sec = 0.0, pi_conc = 0.0 ;
-   
+
    pi_sec  = calcular_integral_secuencial() ;
    pi_conc = calcular_integral_concurrente() ;
-   
-   cout << "valor de PI (calculado secuencialmente)  == " << pi_sec  << endl 
-        << "valor de PI (calculado concurrentemente) == " << pi_conc << endl ; 
-    
+
+   cout << "valor de PI (calculado secuencialmente)  == " << pi_sec  << endl
+        << "valor de PI (calculado concurrentemente) == " << pi_conc << endl ;
+
    return 0 ;
 }
 // ---------------------------------------------------------------------

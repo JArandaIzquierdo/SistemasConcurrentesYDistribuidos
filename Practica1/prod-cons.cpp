c2dfd24145658a6716389e690aaadf2de2ae7a36
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-29 02:06:36 +0100

    solucionado problemas en la funcion consumidor y al inicializar los semaforos)

--- Practica1/prod-cons.cpp
+++ Practica1/prod-cons.cpp
@@ -16,7 +16,7 @@ const unsigned
   num_items  = 40 , // Numero de items a Consumirdor
   tam_vector = 10 ; // Tamaño del vector
   int buffer [tam_vector];  // Buffer
-  int indice = 0;
+  int j = 0;
   sem_t consumir, producir, tex; // Declaracion de los semaforos
 
 // ---------------------------------------------------------------------
@@ -43,14 +43,14 @@ void * productor( void * )
 {
   for( unsigned i = 0 ; i < num_items ; i++ )
   {
-    int dato = producir_dato();  // Producimos un nuevo dato
+    int elemento = producir_dato();  // Producimos un nuevo dato
     sem_wait (&producir); // Semaforo del productor
     sem_wait (&tex);  // Ya que nos encontramos ante una seccion critica
 
     //------------------- Seccion critica ---------------------------
-    cout << "Dato producido: " << dato << endl;
-    buffer[i] = dato; // Escribe el dato en la posicion i del buffer
-    i++;   // Incrementa la posicion del buffer
+    cout << "Dato producido: " << elemento << endl;
+    buffer[j] = elemento; // Escribe el dato en la posicion i del buffer
+    j++;   // Incrementa la posicion del buffer
     // ------------------ Fin seccion critica ------------------------
     sem_post (&tex); // Decrementamos la seccion critica para que otro proceso pueda entrar
     sem_post (&consumir); // Decrementamos el consumidor
@@ -68,15 +68,15 @@ void * consumidor( void * )
       sem_wait(&consumir);
       sem_wait (&tex);
       //------------------- Seccion critica ----------------------------
-      i--;
-      elemento - buffer[indice];
+      elemento = buffer[j-1];
+      j--;
       //------------------- Fin seccion critica ------------------------
       sem_post (&tex);
       sem_post (&producir);
 
 
 
-    consumir_dato (dato) ;
+    consumir_dato (elemento) ;
   }
   return NULL ;
 }
@@ -88,13 +88,13 @@ int main()
   pthread_t hebraConsumidor, hebraProductor;
 
   // Inicializacion de los semaforos
-  sem_init (&consumidor,0,0); // Semaforo para el consumidor
-  sem_init (&producutor,0,num_items); // Semaforo para el productor
+  sem_init (&consumir,0,0); // Semaforo para el consumidor
+  sem_init (&producir,0,num_items); // Semaforo para el productor
   sem_init (&tex,0,1); // Semaforo de exclusion mutua
 
   // Creacion de las hebras
-  pthread_create (&hebraConsumidor,NULL, consumidor, NULL;
-  pthread_create (&hebraProductor,NULL, producutor, NULL;
+  pthread_create (&hebraConsumidor,NULL, consumidor, NULL);
+  pthread_create (&hebraProductor,NULL, producutor, NULL);
 
   // Deja que las hebras terminen de ejecutarse
   pthread_join(hebraProductor,NULL);

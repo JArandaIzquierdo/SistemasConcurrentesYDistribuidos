bfd42468427e42a4c2e640504f3e93ccf8012a45
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-29 01:09:39 +0100

    Modificado la funcion del productor

--- Practica1/prod-cons.cpp
+++ Practica1/prod-cons.cpp
@@ -12,8 +12,8 @@ using namespace std ;
 const unsigned
   num_items  = 40 , // Numero de items a Consumirdor
   tam_vector = 10 ; // Tamaño del vector
-  int buffer [tam_vector];
-  sem_t consumir, producir, mutex;
+  int buffer [tam_vector];  // Buffer
+  sem_t consumir, producir, mutex; // Declaracion de los semaforos
 
 // ---------------------------------------------------------------------
 
@@ -42,8 +42,12 @@ void * productor( void * )
     int dato = producir_dato();  // Producimos un nuevo dato
     sem_wait (&producir); // Semaforo del productor
     sem_wait (&mutex);  // Ya que nos encontramos ante una seccion critica
+
+    //------------------- Seccion critica ---------------------------
+    cout << "Dato producido: " << dato << endl;
     buffer[i] = dato; // Escribe el dato en la posicion i del buffer
     i++;   // Incrementa la posicion del buffer
+    // ------------------ Fin seccion critica ------------------------
     sem_post (&mutex); // Decrementamos la seccion critica para que otro proceso pueda entrar
     sem_post (&consumir); // Decrementamos el consumidor
   }
@@ -53,14 +57,19 @@ void * productor( void * )
 
 void * consumidor( void * )
 {
-  for( unsigned i = 0 ; i < num_items ; i++ )
+  for( unsigned i = 0 ; i < num_items ; i++)
   {
-    int dato ;
+    int elemento;
+    while (true)
+    {
+      sem_wait(&consumidor);
+      sem_wait (&mutex);
+      i--;
+    }
 
-    // falta: leer "dato" desde el vector intermedio
-    // .................
 
-    consumir_dato( dato ) ;
+
+    consumir_dato (dato) ;
   }
   return NULL ;
 }
@@ -68,7 +77,18 @@ void * consumidor( void * )
 
 int main()
 {
+  // Creacion de la hebras
   pthread_t hebraConsumidor, hebraProductor;
+
+  // Inicializacion de los semaforos
+  sem_init (&consumidor,0,0);
+  sem_init (&producutor,0,num_items);
+  sem_init (&mutex,0,1);
+
+  // Creacion de las hebras
+  pthread_create (&hebraConsumidor,null, consumidor, (void *)50);
+  pthread_create (&hebraProductor,null, producutor, 0;
+
 
 
    return 0 ;
